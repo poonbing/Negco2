0d615ff2d17534e6028a174d973245a511b93ac4
poonbing <110159545+poonbing@users.noreply.github.com>
2023-06-24 00:15:33 +0800

    Logic complete

--- __init__.py
+++ __init__.py
@@ -12,35 +12,67 @@ def index():
         session.pop('user', None)
         return render_template('index.html')
 
+
 @app.route('/track', methods=['GET', 'POST'])
 def track():
-    if 'start_tracker' in request.form:
-        print('create')
-        item = request.form.get('item')
-        rate = request.form.get('rate')
-        key = request.form.get('key')
+    try:
         user = session['user']
-        Tracker().update_tracker_session(item, rate, user, key)
-        timers = Tracker().get_active_trackers(user)
-        return render_template('tracker.html', user = user, keylist = timers)
-    if 'user_id' in request.form:
+    except:
         user = request.form.get('user_id')
         session['user'] = user
         timers = Tracker().get_active_trackers(user)
-        return render_template('tracker.html', user = user, keylist = timers)
-    else:
-        user = session['user']
+        return render_template('tracker.html', user=user, keylist=timers)
+    if 'start_tracker' in request.form:
+        name = request.form.get('name')
+        rate = request.form.get('rate')
+        key = request.form.get('key')
+        Tracker().update_tracker_session(name, rate, user, key)
         timers = Tracker().get_active_trackers(user)
-        return render_template('tracker.html', user = user, keylist = timers)
+        return render_template('tracker.html', user=user, keylist=timers)
+    if 'item_form' in request.form:
+        timers = {}
+        index = int(request.form.get('item_index'))-1
+        item = request.form.get('new_item')
+        rate = int(request.form.get('new_rate'))
+        name = request.form.get('new_name')
+        method = request.form.get('form_action')
+        trackers = Tracker().get_active_trackers(user)
+        if method == 'update':
+            count = 0
+            for key in trackers:
+                if count == index:
+                    timers[name] = [item, rate, None, None]
+                else:
+                    timers[key] = trackers[key]
+                count += 1
+        elif method == 'create':
+            timers = trackers
+            timers[name] = [item, rate, None, None]
+        Tracker().set_active_trackers(user, timers)
+        return render_template('tracker.html', user=user, keylist=timers)
+    if 'delete' in request.form:
+        index = request.form.get('index')
+        timers = Tracker().get_active_trackers(user)
+        count = 0
+        timers.pop(index)
+        print(timers)
+        Tracker().set_active_trackers(user, timers)
+        return render_template('tracker.html', user=user, keylist=timers)
+    else:
+        timers = Tracker().get_active_trackers(user)
+        return render_template('tracker.html', user=user, keylist=timers)
+
 
 @app.route('/report', methods=['GET', 'POST'])
 def generate_report():
+    report = Report()
     if request.method == 'POST':
         user = session['user']
-        x_axis, y_axis = Report().generate_datapoints(Report().generate_report(user))
-        items = Report().generate_report(user)
-        return render_template('report.html', list=items, x = x_axis, y = y_axis)
+        x_axis, y_axis = report.generate_datapoints(
+            report.generate_report(user))
+        items = report.generate_report(user)
+        return render_template('report.html', list=items, x=x_axis, y=y_axis)
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
--- functions/tracker.py
+++ functions/tracker.py
@@ -2,11 +2,12 @@ from datetime import datetime
 import shelve
 import uuid
 
+
 class Tracker:
     def __init__(self):
         pass
 
-    #setter & gettter
+    # setter & gettter
 
     def get_active_trackers(self, user):
         try:
@@ -14,7 +15,8 @@ class Tracker:
                 data = db[user]
             return data
         except:
-            data = {'Air Conditioning':['Aircon', 10, None, None], 'Shower':['Shower', 10, None, None], 'Room Lights':['Lights', 10, None, None]}
+            data = {'Air Conditioning': ['Aircon', 10, None, None], 'Shower': [
+                'Shower', 10, None, None], 'Room Lights': ['Lights', 10, None, None]}
             self.set_active_trackers(user, data)
             return data
 
@@ -23,7 +25,8 @@ class Tracker:
             with shelve.open('user') as db:
                 db[user] = data
         except:
-            data = {'Air Conditioning':['Aircon', 10, None, None], 'Shower':['Shower', 10, None, None], 'Room Lights':['Lights', 10, None, None]}
+            data = {'Air Conditioning': ['Aircon', 10, None, None], 'Shower': [
+                'Shower', 10, None, None], 'Room Lights': ['Lights', 10, None, None]}
             self.set_active_trackers(user, data)
 
     def get_tracker_session(self, tracker_id):
@@ -39,15 +42,15 @@ class Tracker:
         with shelve.open(f'tracker{year}{month}') as db:
             db[tracker_id] = data
 
-    #relative functions
+    # relative functions
 
     def stop_active_tracker(self, user, tracker_id):
         tracker_tray = self.get_active_trackers(user)
-        for id in tracker_tray:
-            if id == tracker_id:
-                item = tracker_tray[id][0]
-                rate = tracker_tray[id][1]
-                tracker_tray[id] = [item, rate, None, None]
+        for i in tracker_tray:
+            item = tracker_tray[i][0]
+            if i == tracker_id:
+                rate = tracker_tray[i][1]
+                tracker_tray[i] = [item, rate, None, None]
                 self.set_active_trackers(user, tracker_tray)
                 break
         print(f'{tracker_id} tracker defaulted')
@@ -67,7 +70,8 @@ class Tracker:
             key = str(uuid.uuid4())
             print(key)
             current_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-            data = {'user':user, 'item': item,  'rate':rate,  'time_start': current_time,  'time_end': None}
+            data = {'user': user, 'item': item,  'rate': rate,
+                    'time_start': current_time,  'time_end': None}
             self.set_tracker_session(key, data)
             self.start_active_tracker(user, item, key, rate, current_time)
         elif key != 'None':
@@ -76,4 +80,3 @@ class Tracker:
             data['time_end'] = current_time
             self.set_tracker_session(key, data)
             self.stop_active_tracker(user, item)
-
