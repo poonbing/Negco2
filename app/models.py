8130d34dc5c9a827dd1c1ff7eb9925f14f3d8e56
DeviousDooDooDevourer <110159545+poonbing@users.noreply.github.com>
2023-07-02 21:15:36 +0800

    Fixed Models according to Mysql

--- app/models.py
+++ app/models.py
@@ -12,18 +12,18 @@ from .extensions import db
 class User(db.Model, UserMixin):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.INTEGER, nullable = False, primary_key=True)
     profile_picture = db.Column(db.String(200), unique=True)
-    username = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(50))
-    role = db.Column(db.String(20))
-    email = db.Column(db.String(100), unique=True)
-    gender = db.Column(db.String(10))
-    first_name = db.Column(db.String(50))
-    last_name = db.Column(db.String(50))
-    age = db.Column(db.Integer)
-    phone = db.Column(db.String(15))
-    login_attempts = db.Column(db.Integer, default=0)
+    username = db.Column(db.String(50), nullable = False, unique=True)
+    password = db.Column(db.String(50), nullable = False)
+    role = db.Column(db.String(20), nullable = False)
+    email = db.Column(db.String(100), nullable = False, unique=True)
+    gender = db.Column(db.String(10), nullable = False)
+    first_name = db.Column(db.String(50), nullable = False)
+    last_name = db.Column(db.String(50), nullable = False)
+    age = db.Column(db.INTEGER, nullable = False)
+    phone = db.Column(db.String(15), nullable = False)
+    login_attempts = db.Column(db.INTEGER, default=0)
 
     locked = db.relationship("LockedUser", backref="user", uselist=False)
 
@@ -79,8 +79,8 @@ class User(db.Model, UserMixin):
 class LockedUser(db.Model):
     __tablename__ = "locked_users"
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), unique=True)
+    id = db.Column(db.INTEGER, nullable = False, primary_key=True)
+    user_id = db.Column(db.INTEGER, db.ForeignKey("users.id"), nullable = False, unique=True)
     locked_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def is_locked(self):
@@ -91,16 +91,16 @@ class Tracker(db.Model):
     __tablename__ = 'tracker'
     #sql model
     id = db.Column(db.String(36), primary_key = True, unique = True)
-    user_id = db.Column(db.String(36), nullable = False)
+    user = db.Column(db.INTEGER, nullable = False)
     name = db.Column(db.String(45), nullable = False)
     item = db.Column(db.String(45), nullable = False)
     rate = db.Column(db.INTEGER, nullable = False)
     start_time = db.Column(db.String(20), nullable = False)
     end_time = db.Column(db.String(20))
 
-    def __init__(self, id, user_id, name, item, rate, start_time, end_time):
+    def __init__(self, id, user, name, item, rate, start_time, end_time):
         self.id = id
-        self.user_id = user_id
+        self.user = user
         self.name = name
         self.item = item
         self.rate = rate
@@ -110,41 +110,22 @@ class Tracker(db.Model):
     def to_dict(self):
         return {
             'id': self.id,
-            'user_id': self.user_id,
+            'user_id': self.user,
             'name': self.name,
             'item': self.item,
             'rate': self.rate,
             'start_time': self.start_time,
             'end_time': self.end_time
         }
-    
-class SessionTracker(db.Model):
-    __tablename__ = 'session_tracker'
-    #sql model
-    user_id = db.Column(db.String(36), primary_key = True, unique = True)
-    active_sessions = relationship('SessionInfo', backref='session_tracker')
-
-    def __init__(self, user_id, session_info=None):
-        self.user_id = user_id
-        self.session_info = session_info or []
-
-    def add_session_info(self, data):
-        self.session_info.append(data)
-    
-    def to_dict(self):
-        return {
-            'user_id':self.user_id,
-            'active_session':self.active_sessions
-        }
 
 class SessionInfo(db.Model):
     __tablename__ = 'session_info'
 
-    id = db.Column(db.String(36), primary_key = True, unique = True)
-    active_sessions = db.Column(db.String(45), ForeignKey('session_tracker.user_id'))
+    id = db.Column(db.String(36), primary_key = True, nullable = False, unique = True)
+    active_sessions = db.Column(db.String(45), ForeignKey('session_tracker.user_id'), nullable = False)
     name = db.Column(db.String(45), nullable = False)
     item = db.Column(db.String(45), nullable = False)
-    session_id = db.Column(db.String(36), nullable = False)
+    session_id = db.Column(db.String(36))
     session_start = db.Column(db.String(20))
     rate = db.Column(db.INTEGER, nullable = False)
 
