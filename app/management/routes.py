dd0ea4163853dcaaac953d8961de7522440dd81a
youshen52 <122770139+youshen52@users.noreply.github.com>
2023-08-10 10:42:16 +0800

    Testing attention pls

--- app/management/routes.py
+++ app/management/routes.py
@@ -15,8 +15,8 @@ from io import BytesIO
 # Local Modules
 from app import limiter
 from app.management import bp
-from .utils import role_required, compress_and_resize
-from ..models import User, LockedUser, Session
+from .utils import role_required, compress_and_resize, generate_api_key
+from ..models import User, LockedUser, Session, APIKey
 from ..extensions import db
 from ..forms import SettingsForm, UnlockAccountForm
 from ..models import User
